96960b67427d7deb1574ab294cad028a9d58b015
Mihai <mihai.giosan.2020@gmail.com>
2021-06-22 01:17:32 +0300

    add starter files

---
+++ src/main/java/ro/salty/saltyBot/SaltyBotApplication.java
@@ -0,0 +1,13 @@
+package ro.salty.saltyBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaltyBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaltyBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/salty/saltyBot/SaltyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.salty.saltyBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaltyBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
